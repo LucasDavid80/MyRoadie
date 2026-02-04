61643ad101341016933731117e57cc22e73f8577
Lucas David <lucasdavidoliveira80@gmail.com>
2026-02-03 22:19:06 -0300

    refactor(AppColors): Criaação de um documento para centralizar as cores do app

---
+++ lib/presentation/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFf59e0b); // Laranja
+  static const Color secondary = Color(0xFF2c3e50); // Azul Escuro
+  static const Color background = Colors.white;
+  static const Color inputBorder = Color(0xFFE0E0E0);
+}
